b30038542f356670b4ef16ec3e5a3efcbce862a1
Pavan Chanduri <pavcha@amazon.com>
2025-09-02 13:21:26 +0530

    first commit

---
+++ src/StockBuySellDifferentDay.java
@@ -0,0 +1,23 @@
+public class StockBuySellDifferentDay {
+    public static void main(String[] args) {
+        int[] prices = {7,6,4,3,1};
+        System.out.println(sellBuyStock(prices));
+    }
+
+    private static int sellBuyStock(int[] prices) {
+        int maxProfit = 0;
+        int profit = 0;
+        int buy_price = prices[0];
+        for(int i=1;i<prices.length;i++) {
+            if(prices[i]<buy_price) {
+                buy_price = prices[i];
+            } else {
+                int sell_price = prices[i];
+                profit = sell_price - buy_price;
+            }
+            maxProfit = Math.max(profit, maxProfit);
+        }
+        return maxProfit;
+    }
+    
+}
---
+++ src/TwoSum.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        if (result != null) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No two sum solution found.");
+        }
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+            if (map.containsKey(difference)) {
+                return new int[] { map.get(difference), i };
+            }
+            map.put(nums[i], i);
+        }
+        return null; // Return null if no solution is found
+    }
+}
