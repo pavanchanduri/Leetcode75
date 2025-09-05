da709c90dccbd8d180abc9c5e103837c9f2acf96
Pavan Chanduri <pavcha@amazon.com>
2025-09-05 07:41:45 +0530

    Modified logic for climb stairs

--- src/arrays/ClimbStairs.java
+++ src/arrays/ClimbStairs.java
@@ -11,21 +11,28 @@ public class ClimbStairs {
      * The algorithm works as below:
      * 1. If there is 1 step, there is 1 way to climb it.
      * 2. If there are 2 steps, there are 2 ways to climb it.
-     * 3. For more than 2 steps, the number of ways to reach the nth step 
+     * 3. For more than 2 steps, the number of ways to reach the nth step
      * is the sum of the ways to reach the (n-1)th and (n-2)th steps.
-     * 
+     *
      * @param n
      * @return
      */
     private static int climbStairs(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
+        if (n == 1)
+            return 1;
+        int one = 1;
+        int two = 2;
+        int result = 0;
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            result = one + two;
+            one = two;
+            two = result;
         }
-        return dp[n];
+        /**
+         * We return two because we are starting from the third step.
+         * If we start from the first step, we would need to return one.
+         * If n==2, we need to return two and not total.
+         */
+        return two;
     }
 }
